b1ee902524d490ff807a87f47247bc5742e27288
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2024-03-19 19:09:45 -0400

    working version form Justin

--- PhaT_main/IO.ino
+++ PhaT_main/IO.ino
@@ -1,4 +1,3 @@
-
 void init_io() {
 
   pinMode(LED_BUILTIN, OUTPUT);
@@ -34,4 +33,64 @@ int SPI_PT_WRITE(int PCommand){
 void timer_function(){
     //Serial.println(">");
     count++;
+}
+
+unsigned char crc4(unsigned int n_prom[]) { 
+  int cnt;      // simple counter
+  unsigned int n_rem;     // crc reminder
+  unsigned int crc_read;    // original value of the crc
+  unsigned char
+  n_bit;  n_rem = 0x00;
+  crc_read=n_prom[7];     //save read CRC 
+  n_prom[7]=(0xFF00 & (n_prom[7]));   //CRC byte is replaced by 0
+  for (cnt = 0; cnt < 16; cnt++)      // operation is performed on bytes
+  { // choose LSB or MSB
+    if (cnt%2==1) n_rem ^= (unsigned short) ((n_prom[cnt>>1]) & 0x00FF);
+    else n_rem ^= (unsigned short) (n_prom[cnt>>1]>>8);
+    for (n_bit = 8; n_bit > 0; n_bit--)
+    {               
+      if (n_rem & (0x8000))
+      {    
+        n_rem = (n_rem << 1) ^ 0x3000; 
+      }
+      else  
+      { 
+        n_rem = (n_rem << 1); 
+      }         
+    }      
+  }  
+n_rem=  (0x000F & (n_rem >> 12)); // // final 4-bit reminder is CRC code  
+n_prom[7]=crc_read;   // restore the crc_read to its original place      
+return (n_rem ^ 0x00); 
+}
+
+unsigned int SumArray(int Error[]) {
+  int sum = 0;
+  for (int i=0; i<=15; i++){
+    Serial.print(Error[i]);
+    sum = sum + Error[i];
+  }
+  return sum; 
+}
+
+void I2C_RxHandler(int numBytes)
+{
+  RxByte = Wire.read();
+  if (RxByte=28) {
+    DataFile = SD.open("PHaTData.txt");
+    while(Wire.available()) {  // Read Any Received Data
+      RxByte = Wire.read();
+    }
+  } else if (RxByte=61) {
+    APRSFile = SD.open("APRSData.txt");
+    while(Wire.available()) {  // Read Any Received Data
+      RxByte = Wire.read();
+    }
+  } else{
+  }
+}
+
+void I2C_TxHandler(void)
+{
+  Wire.write(TxByte);
 }
--- PhaT_main/PhaT_main.ino
+++ PhaT_main/PhaT_main.ino
@@ -1,195 +1,322 @@
+//// Global variables
 
-// Global variables
-
+//Macro Variables
+byte TxByte = 0;
+byte RxByte;
+String FileTitle = "PHaTData";
+String FileFormat = ".txt";
+String FileName;
+int Error[16];
+bool Flag[16];
+unsigned int ErrorSum;
+// Variable for RTD sensor
+float Ext_Temperature = 0;
+// Variable for Pressure Sensor
 int PCommand=0;
-bool Error = 0;
+byte P_Data1;
+byte P_Data2;
+byte P_Data3;
+unsigned short n_prom[8];
+unsigned int n_rem;
+unsigned long UCTemperature;
+unsigned long UCPressure;
+int SPIRate = 100000;
+//
+// Variables for 555 timer/RH sensor
+float B5 = 0.0014;    // 1/degree C
+float B2 = 0.1325;    // %RH/degree C
+float B3 = -0.0317;   // unitless
+float B4 = -3.0876;   // %RH
 int count = 0;
+int count_cap = 0;
+int count_delay = 500;     // number of miliseconds to count 555 timer interupts
+float f_ref = 3.6E3;     // The number of interupts detected in 0.5 seconds at 30% RH
+float c_ref = 650;     // The capacitance of RH sensor at 30% RH in pF
+float RH_const = f_ref*c_ref;
+float c_humid = 0.0;
+float RH_value = 30.0;
+float Change_RH = 0;
+//
+// Varables for Voltage Reader
 float calibrationFactor = 0.00488*1.769; // Calibration*VoltageDivider
 float voltage;
 float pinvoltage;
-#include <Wire.h>
+//
+#include "Wire.h"
 #include <Adafruit_MAX31865.h>
-#include <SPI.h>
+#include "SPI.h"
 #include "PinChangeInterrupt.h"
-#include <SD.h>
+#include "SD.h"
 #define Trigger_PIN  7
-
-#define sd 1
+// Independent Parts, a 0 will remove that part from the code
+#define sd 1           // should always be 1 or code won't work
 #define RTD 1
-// Use software SPI: CS, DI, DO, CLK
+#define PS 1
+#define RH 1
+#define V 1
+#define Time 1
 
 #ifdef RTD
 Adafruit_MAX31865 max = Adafruit_MAX31865(3);
 float RREF = 4300.0;
+float RNOMINAL = 1000.0;
 #endif
 
 #ifdef sd
-File TestFile;
+File DataFile;    // File name the code will know to write to
+File APRSFile;
 #endif
 
 void setup() {
-  // put your setup code here, to run once:
 
-// Setup for RTD
+  Wire.begin(0x55); // Initialize I2C (Slave Mode: address=0x55 )
+  Wire.onReceive(I2C_RxHandler);
+  Wire.onRequest(I2C_TxHandler);
+
   init_io();
   Serial.begin(9600);
   Serial.println("PHaT - Start");
 
-#ifdef sd
-  Serial.println("SD Card Printing");
-#endif
-  ///// Setup for Pressure Sensor ///////
-/*
-  SPI.begin();
-  SPI_PT_WRITE(PCommand=0xAE);
-  short Output;
-  Output = SPI_PT_WRITE(a=0xA7);
-  Serial.println(Output);
-  Output = SPI_PT_WRITE(a=0x00);
-  Serial.println(Output);
-  Output = SPI_PT_WRITE(a=0x00);
-  Serial.println(Output);
-  digitalWrite(5, HIGH);
-  SPI.endTransaction();
-*/
- //////////// Setup for SD Card ////////////////
-#ifdef sd
-  const int CSSD = 4;
-  //digitalWrite(4, LOW);
-  //Serial.print("Initializing SD card...");
+  #ifdef RTD
+    max.begin(MAX31865_3WIRE);
+    Serial.println("PHaT - 1");
+  #endif
 
-  if (!SD.begin(CSSD)) {
-    //Serial.println("initialization failed!");
-    Error = 1;
-  }
-  //Serial.println("initialization done.");
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  TestFile = SD.open("test2.txt", FILE_WRITE);
+    ///// Setup for Pressure Sensor ///////
+  #ifdef PS
+    SPI.begin();
+    SPI.beginTransaction(SPISettings(SPIRate, MSBFIRST, SPI_MODE0));
+    digitalWrite(5, LOW);
+    delayMicroseconds(50);
+    SPI.transfer(0x1E);
+    delay(3);
+    digitalWrite(5, HIGH);
+    delay(1);
 
-  // if the file opened okay, write to it:
-  if (TestFile) {
-    //Serial.print("Writing to test.txt...");
-    //TestFile.println("testing 1, 2, 3.");
-    // close the file:
-    TestFile.close();
-    //Serial.println("done.");
-  } else {
-    // if the file didn't open, print an error:
-    //Serial.println("error opening test.txt");
-    Error = 1;
-      // re-open the file for reading:
-  }
-  //TestFile = SD.open("test.txt");
-  //if (TestFile) {
-  //  Serial.println("test.txt:");
+    Serial.println("PHaT - 2");
 
-    // read from the file until there's nothing else in it:
-    //while (TestFile.available()) {
-    //  Serial.write(TestFile.read());
+    for(int i=0;i<8;i++){
+    unsigned short a = 0xA0+i*2;
+    digitalWrite(5, LOW);
+    delayMicroseconds(50);
+    SPI.transfer(a);
+    n_prom[i] = SPI.transfer16(0x00);
+    delayMicroseconds(50);
+    digitalWrite(5, HIGH);
+    SPI.endTransaction();
+    delay(1);
+
+    }
+
+    n_rem = crc4(n_prom);
+    if (n_rem != 15){
+      Error[0] = 1;
+    }
+  #endif
+
+  Serial.println("PHaT - 3");
+  Serial.println(Error[0]);
+  //////////// Setup for SD Card ////////////////
+  #ifdef sd
+    const int CSSD = 4;
+    digitalWrite(4, LOW);
+    //Serial.println("Initializing SD card...");
+
+    if (!SD.begin(CSSD)) {
+      Error[1] = 1;
+      Serial.println(Error[1]);
+    }
+    // open the file. note that only one file can be open at a time,
+    // so you have to close this one before opening another.
+    int m = 0;
+    FileName = FileTitle + FileFormat;
+    //while (SD.exists(FileName) == true){
+    //  m=m+1;
+    // FileName = FileTitle + FileFormat;
     //}
-    // close the file:
-  //  TestFile.close();
-  //} else {
-    // if the file didn't open, print an error:
-  //  Serial.println("error opening test.txt");
-  //  Error = 1;
-  //}
-#endif
-  if (Error){
-    digitalWrite(A3, HIGH);
-    delay(1000);
-  } else {
-    digitalWrite(A3, HIGH);
-    delay(80);
-    digitalWrite(A3, LOW);
-    delay(80);
-    digitalWrite(A3, HIGH);
-    delay(80);
-    digitalWrite(A3, LOW);
-  }
+    //FileName = "PHaT_Data_"+m;
+    Serial.println(FileName);
+    DataFile = SD.open(FileName, FILE_WRITE);    // Actual name of the file
 
+    // if the file opened okay, write to it:
+    if (DataFile) {
+      DataFile.println("External Temp[C], Count, Humidity[%RH], Pressure[Pa], Internal Temp[C], Time[s], Batt Voltage[V], Pin Voltage[V], Flags");
+      // close the file:
+      Serial.println("PHaT - 4");
+      DataFile.close();
+    } else {
+      // if the file didn't open, print an error:
+      Error[2] = 1;
+      Serial.println(Error[2]);
+        // re-open the file for reading:
+    }
+    DataFile = SD.open(FileName);
+    if (DataFile) {
+      Serial.println("PHaT - 5");
+      // close the file:
+      DataFile.close();
+    } else {
+      // if the file didn't open, print an error:
+      Error[3] = 1;
+    }
+
+    ErrorSum = SumArray(Error);
+    Serial.println(ErrorSum);
+    if (ErrorSum > 0){
+      Serial.println("PHaT - 7");
+      digitalWrite(A3, HIGH);
+      delay(20);
+      digitalWrite(A3, LOW);
+    } else {
+      Serial.println("PHaT - 8");
+      digitalWrite(A3, HIGH);
+      delay(20);
+      digitalWrite(A3, LOW);
+      delay(20);
+      digitalWrite(A3, HIGH);
+      delay(20);
+      digitalWrite(A3, LOW);
+    }
+  #endif
 }
 
 void loop() {
   #ifdef sd
-    TestFile = SD.open("test.txt", FILE_WRITE);
+    DataFile = SD.open(FileName, FILE_WRITE);
   #endif
+
+  /////////// Loop for RTD sensor //////////////
+  #ifdef RTD
+    //Serial.println("RTD Start");
+    float rtd = max.readRTD();
+    float ratio = rtd;
+    ratio /= 32768;
+    Ext_Temperature = max.temperature(1000, RREF);
+
+    //Serial.println(count);
+
+    digitalWrite(A2, HIGH);
+    DataFile.print(Ext_Temperature); //More accurate way to do this
+    digitalWrite(A2, LOW);
+
   // loop for RH            // If battery lower than 6.5V the frequency starts to drop below 12KHz
                                        // For all other battery power the frequency is stable at 12KHz
-  
-  digitalWrite(A0, HIGH);
-  Serial.println("Saving Count");
-  //TestFile.print("Count, ");
-  //TestFile.println(count);
-  digitalWrite(A0, LOW);
-  count = 0;
-  delay(1000);
+  #ifdef RH
+    //Serial.println("RH Start");
+    count=0;
+    digitalWrite(A0, HIGH);
+    Serial.print("");  // This is needed before the delay for some reason. Not sure why, but if it's not there count is zero
+    delay(count_delay);
+    count_cap = count;
+    digitalWrite(A0, LOW);
+    //Serial.println(count_cap);
+    //Serial.println(RH_const);
+    //delay(1000);
+    c_humid = RH_const/count_cap;
+    RH_value = 30 + 0.909091 * (c_ref - c_humid);
+    Change_RH = (B5*RH_value+B2)*Ext_Temperature + (B3*RH_value+B4);
+    RH_value = RH_value + Change_RH;
+    //Serial.println(c_humid);
+    //Serial.println(RH_value);
+    DataFile.print(", ");
+    DataFile.print(count_cap);
+    DataFile.print(", ");
+    DataFile.print(RH_value); 
+
+  #endif
+
+  // Loop for Pressure Sensor
+  #ifdef PS
+    //Serial.println("PS Start");
+    for(int i=0;i<2;i++){
+      unsigned short a = 0x48+i*10;
+      digitalWrite(5, LOW);
+      delayMicroseconds(50);
+      SPI.transfer(a);
+      delay(9);
+      digitalWrite(5, HIGH);
+      delay(1);
+      digitalWrite(5, LOW);
+      delayMicroseconds(50);
+//      for(int j=0;j<3;j++){
+//        P_Data[i][j] = SPI.transfer(0x00);
+//      }
+      P_Data1 = SPI.transfer(0x00);
+      P_Data2 = SPI.transfer(0x00);
+      P_Data3 = SPI.transfer(0x00);
+      delayMicroseconds(50);
+      digitalWrite(5, HIGH);
+      SPI.endTransaction();
+      delay(1);
+    }
+    Serial.print("1 : ");
+    Serial.println(P_Data1);
+    Serial.print("2 : ");
+    Serial.println(P_Data2);
+    Serial.print("3 : ");
+    Serial.println(P_Data3);
+    DataFile.print(", ");
+    DataFile.print("N/A");
+    DataFile.print(", ");
+    DataFile.print("N/A");
+  #endif
+
+  // Loop for Time of Sample
+  #ifdef Time
+    //Serial.println("Time Start");
+    DataFile.print(", ");
+    DataFile.print("N/A");
+  #endif
+
+    // Check and print any faults
+    uint8_t fault = max.readFault();
+    if (fault) {
+      if (fault & MAX31865_FAULT_HIGHTHRESH) {
+        Flag[0] = 1; // Serial.println("RTD High Threshold"); 
+      }
+      if (fault & MAX31865_FAULT_LOWTHRESH) {
+        Flag[1] = 1; //Serial.println("RTD Low Threshold"); 
+      }
+      if (fault & MAX31865_FAULT_REFINLOW) {
+        Flag[2] = 1; //Serial.println("REFIN- > 0.85 x Bias"); 
+      }
+      if (fault & MAX31865_FAULT_REFINHIGH) {
+        Flag[3] = 1; //Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+      }
+      if (fault & MAX31865_FAULT_RTDINLOW) {
+        Flag[4] = 1; //Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+      }
+      if (fault & MAX31865_FAULT_OVUV) {
+        Flag[5] = 1; //Serial.println("Under/Over voltage"); 
+      }
+      max.clearFault();
+    }
+  #endif
 
   // Check Battery Voltage
-
-  voltage = analogRead(A7) * calibrationFactor;     // Voltage can be read down to a battery power of 6.5 V
-  pinvoltage = analogRead(A7) * 0.00488;
-  digitalWrite(A1, HIGH);
-  //Serial.println("Saving Voltage");
-  Serial.print(F("Battery[V], "));
-  Serial.println(voltage);
-
-  //TestFile.print(F("Pin[V], "));
-  //TestFile.println(pinvoltage);
-  digitalWrite(A1, LOW);
-
-  // Loop for Pressure
-/*
-  short Output;
-  Output = SPI_PT_WRITE(a=0x02);
-  digitalWrite(5, HIGH);
-  SPI.endTransaction();
-  Serial.println(Output);
-*/
-/////////// Loop for RTD sensor //////////////
-#ifdef RTD
-  float rtd = max.readRTD();
-  Serial.print("RTD value: "); Serial.println(rtd);
-  float ratio = rtd;
-  ratio /= 32768;
-
-  //Serial.print("Ratio = "); Serial.println(ratio,8);
-  //Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
-  digitalWrite(A2, HIGH);
-  //Serial.println("Saving Temperature");
-  //Serial.print("Temp[C], ");
-  //Serial.println(max.temperature(1000, RREF)); //More accurate way to do this
-  digitalWrite(A2, LOW);
-
-  // Check and print any faults
-  uint8_t fault = max.readFault();
-  if (fault) {
-    Serial.print("Fault 0x"); Serial.println(fault, HEX);
-    if (fault & MAX31865_FAULT_HIGHTHRESH) {
-      Serial.println("RTD High Threshold"); 
+  #ifdef V
+    //Serial.println("Voltage Start");
+    voltage = analogRead(A7) * calibrationFactor;     // Voltage can be read down to a battery power of 6.5 V
+    pinvoltage = analogRead(A7) * 0.00488;
+    digitalWrite(A1, HIGH);
+    DataFile.print(", ");
+    DataFile.print(voltage);
+    DataFile.print(", ");
+    DataFile.print(pinvoltage);
+    digitalWrite(A1, LOW);
+    if (voltage<6.5){
+      Flag[6] = 1;
     }
-    if (fault & MAX31865_FAULT_LOWTHRESH) {
-      Serial.println("RTD Low Threshold"); 
-    }
-    if (fault & MAX31865_FAULT_REFINLOW) {
-      Serial.println("REFIN- > 0.85 x Bias"); 
-    }
-    if (fault & MAX31865_FAULT_REFINHIGH) {
-      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
-    }
-    if (fault & MAX31865_FAULT_RTDINLOW) {
-      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
-    }
-    if (fault & MAX31865_FAULT_OVUV) {
-      Serial.println("Under/Over voltage"); 
-    }
-    max.clearFault();
+  #endif
+  //Serial.println(count);
+  //Serial.println("Flag Start");
+  DataFile.print(", ");
+  for (int i=0 ; i<=15; i++){
+    DataFile.print(Flag[i]);
   }
-#endif
-
+    DataFile.println("");
   ////////////
-#ifdef sd
-  TestFile.close();
-#endif
+  #ifdef sd
+    DataFile.close();
+  #endif
 }
